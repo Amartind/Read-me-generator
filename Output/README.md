8b414b57e802c5527962e89bfa0283921f793b73
Amartind <112136243+Amartind@users.noreply.github.com>
2023-01-23 15:43:21 -0800

    update everything

---
+++ Output/README.md
@@ -0,0 +1,61 @@
+# <README Developer> MIT
+
+    ## Description
+    
+    This will help streamline making a readme file so that we can focus on more important things on the project
+    
+    ## Table of Contents (Optional)
+    
+    
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Credits](#credits)
+    - [License](#license)
+    
+    ## Installation
+    
+    You will need to have these dependancies:
+    npm install
+    
+    ## Usage
+    
+    Provide instructions and examples for use. Include screenshots as needed.
+    
+    You need to install the dependencies to have this work
+
+    To add a screenshot, create an 'assets/images'folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+    
+        '''md
+        ![alt text](assets/images/screenshot.png)
+        '''
+    
+    ## Credits
+    
+    No collaborators
+    
+    ## License
+    
+    MIT
+    ---
+    
+    🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+    
+    
+    ## Features
+    
+    If your project has a lot of features, list them here.
+    
+    ## How to Contribute
+    
+    [Contributor Covenant](https://www.contributor-covenant.org/)
+    Please don't
+    ## Tests
+    
+    npm tests
+
+    ## Questions
+
+    If there are any questions about this feel free to contact me :
+    https://www.github.com/Amartind
+    yourampmfriend@hotmail.com
+    
--- index.js
+++ index.js
@@ -1,5 +1,68 @@
 const inquire = require("inquirer")
 const fs = require('fs')
+const readme = (username,email,project,description,license,idepend,rtests,usage,contribute,credits) =>{
+    return `# <${project}> ${license}
+
+    ## Description
+    
+    ${description}
+    
+    ## Table of Contents (Optional)
+    
+    
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Credits](#credits)
+    - [License](#license)
+    
+    ## Installation
+    
+    You will need to have these dependancies:
+    ${idepend}
+    
+    ## Usage
+    
+    Provide instructions and examples for use. Include screenshots as needed.
+    
+    ${usage}
+
+    To add a screenshot, create an 'assets/images'folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+    
+        '''md
+        ![alt text](assets/images/screenshot.png)
+        '''
+    
+    ## Credits
+    
+    ${credits}
+    
+    ## License
+    
+    ${license}
+    ---
+    
+    🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+    
+    
+    ## Features
+    
+    If your project has a lot of features, list them here.
+    
+    ## How to Contribute
+    
+    [Contributor Covenant](https://www.contributor-covenant.org/)
+    ${contribute}
+    ## Tests
+    
+    ${rtests}
+
+    ## Questions
+
+    If there are any questions about this feel free to contact me :
+    https://www.github.com/${username}
+    ${email}
+    `
+}
 inquire
     .prompt([
         {
@@ -23,9 +86,10 @@ inquire
             name:'description',
         },
         {
-            type:"input",
+            type:"list",
             message:"What kind of license should your project have?",
             name:'license',
+            choices:["MIT","GNU Public license v3.0","Apache License 2.0","Please refer to to license file"]
         },
         {
             type:"input",
@@ -53,28 +117,7 @@ inquire
             name:'credits',
         },
     ])
-    .then ( (response) => {console.log(response.username + response.email + response.project + response.description + response.license
-         + response.idepend + response.rtests + response.usage + response.contribute);
-        // fs.writeFileSync("Read-me.md", JSON.stringify(response, null, '\t'), (err) =>
-        // err ? console.log(err) : console.log('Success!'))
-        fs.writeFileSync("Read-me.md", `#${JSON.stringify(response.project)} \n`, (err) => {
-        err ? console.error(err) : console.log('Data has been appended to the file successfully.')})
-        fs.appendFileSync("Read-me.md", `## Description \n ${JSON.stringify(response.description)} \n`, (err) => {
-        err ? console.error(err) : console.log('Data has been appended to the file successfully.')})
-        fs.appendFileSync("Read-me.md", `## Installation \n ${JSON.stringify(response.idepend)} \n`, (err) => {
-        err ? console.error(err) : console.log('Data has been appended to the file successfully.')})
-        fs.appendFileSync("Read-me.md", `## Usage \n ${JSON.stringify(response.usage)} \n`, (err) => {
-        err ? console.error(err) : console.log('Data has been appended to the file successfully.')})
-        fs.appendFileSync("Read-me.md", `## Credits \n ${JSON.stringify(response.credits)} \n`, (err) => {
-        err ? console.error(err) : console.log('Data has been appended to the file successfully.')})  
-        fs.appendFileSync("Read-me.md", `## License \n ${JSON.stringify(response.license)} \n`, (err) => {
-        err ? console.error(err) : console.log('Data has been appended to the file successfully.')})
-        fs.appendFileSync("Read-me.md", `## How to contribute \n ${JSON.stringify(response.contribute)} \n`, (err) => {
-        err ? console.error(err) : console.log('Data has been appended to the file successfully.')})
-        fs.appendFileSync("Read-me.md", `## Tests \n ${JSON.stringify(response.rtests)} \n`, (err) => {
-        err ? console.error(err) : console.log('Data has been appended to the file successfully.')})
-        fs.appendFileSync("Read-me.md", `## Contact information \n ${JSON.stringify(response.username)} \n`, (err) => {
-        err ? console.error(err) : console.log('Data has been appended to the file successfully.')})
-        fs.appendFileSync("Read-me.md", `${JSON.stringify(response.email)} \n`, (err) => {
-        err ? console.error(err) : console.log('Data has been appended to the file successfully.')})
-        });
+    .then ( (response) => {
+        let filledForm = readme(response.username,response.email,response.project,response.description,response.license,response.idepend,response.rtests,response.usage,response.contribute,response.credits);
+        fs.writeFile("./Output/README.md", filledForm , err => err?console.log(err) :"")
+    });
